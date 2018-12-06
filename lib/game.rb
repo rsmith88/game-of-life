3fd1041327d97e8b8891bd1dda9de08c32a8ca8c
Raylene <smith.raylene@gmail.com>
2018-12-06 21:32:32 +0000

    Added create method to build game

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,20 @@ class Game
   def initialize(row, column)
     @row = row
     @column = column
+    @cells = []
   end
 
-  attr_reader :row, :column
+  attr_reader :row, :column, :cells
+
+  def create
+    @row.times do |row_number|
+      @column.times do |column_number|
+        cell = Cell.new(row_number, column_number)
+        @cells.push(cell)
+        print "X"
+      end
+      puts ""
+    end
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,19 @@
 require 'game'
 
 describe Game do
-  subject = Game.new(5, 6)
+  subject = Game.new(5, 5)
 
   describe '#initialize' do
     it 'takes a number of rows and columns to construct game' do
       expect(subject.row).to eq(5)
-      expect(subject.column).to eq(6)
+      expect(subject.column).to eq(5)
+    end
+  end
+
+  describe '#create' do
+    it 'creates a game with rows and columns of cells' do
+      subject.create
+      expect(subject.cells.length).to eq (25)
     end
   end
 
